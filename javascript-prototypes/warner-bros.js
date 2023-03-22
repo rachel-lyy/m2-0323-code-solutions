a15e1177bb67f34814483e2a401ef5116bfee65a
Rachel Ly <rachel.ly17@gmail.com>
2023-03-22 02:24:35 +0000

    Completed javascript-prototypes exercise.

---
+++ javascript-prototypes/warner-bros.js
@@ -0,0 +1,32 @@
+/* exported yakko, wakko, dot */
+
+const yakko = {
+  age: 14,
+  name: 'Yakko',
+  role: 'brother'
+};
+
+const wakko = {
+  age: 11,
+  name: 'Wakko',
+  role: 'brother'
+
+};
+
+const dot = {
+  age: 10,
+  name: 'Dot',
+  role: 'sister'
+};
+
+const warnerPrototype = {
+  describe: function () {
+    const description = this.name + ' is a ' +
+      this.age + '-year-old Warner ' + this.role + '.';
+    return description;
+  }
+};
+
+Object.setPrototypeOf(dot, warnerPrototype);
+Object.setPrototypeOf(wakko, warnerPrototype);
+Object.setPrototypeOf(yakko, warnerPrototype);
