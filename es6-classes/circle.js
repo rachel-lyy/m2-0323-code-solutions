b0f18ee335ca64bd4b15248d6a590385fdee6c4a
Rachel Ly <rachel.ly17@gmail.com>
2023-03-26 20:10:56 +0000

    Made changes to circle, shape and square files.

--- es6-classes/circle.js
+++ es6-classes/circle.js
@@ -1,16 +1,18 @@
 /* eslint-disable no-undef -- Shape is imported by .html */
 /* exported Circle */
-class Circle extends Square {
-  constructor(area, width, circumference, radius) {
-    super(area, width, circumference);
+class Circle extends Shape {
+  constructor(radius) {
+    const area = Math.round(Math.PI * (radius * radius));
+    const circumference = Math.round(Math.PI * 2 * radius);
+    super(area, circumference);
     this.radius = radius;
   }
 
-  printRadius() {
-    const value = this.printSquare() + ' Radius is ' + this.radius;
-    return value;
+  print() {
+    const circleValue = super.print() + ' Radius is ' + this.radius;
+    return circleValue;
   }
 }
 
-const newCircle = new Circle(23, 34, 6, 10);
-console.log(newCircle.printRadius());
+const newCircle = new Circle(3);
+console.log(newCircle.print());
--- es6-classes/shape.js
+++ es6-classes/shape.js
@@ -13,18 +13,3 @@ class Shape {
 
 const newShape = new Shape(23, 34);
 console.log(newShape.print());
-
-class Square extends Shape {
-  constructor(area, circumference, width) {
-    super(area, circumference);
-    this.width = width;
-  }
-
-  printSquare() {
-    const value = this.print() + ' Width is: ' + this.width;
-    return value;
-  }
-}
-
-const newSquare = new Square(23, 34, 6);
-console.log(newSquare.printSquare());
