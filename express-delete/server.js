9a67780b26aa6d39bb254a9ae9083a447596b917
Rachel Ly <rachel.ly17@gmail.com>
2023-05-11 22:40:48 +0000

    Completed express-delete exercise.

---
+++ express-delete/server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+const app = express();
+const port = 8080;
+var allGrades = [];
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.get('/api/grades', (req, res) => {
+  for (const x in grades) {
+    allGrades.push(grades[x]);
+  }
+  res.json(grades);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  const findRecord = req.params.id;
+  delete grades[findRecord];
+  res.json(grades);
+  res.status(204);
+});
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
+});
