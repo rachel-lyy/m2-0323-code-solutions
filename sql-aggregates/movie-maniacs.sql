784da9c9cc1e6c31c41c817a1452029ecc5db01a
Rachel Ly <rachel.ly17@gmail.com>
2023-05-26 02:46:00 +0000

    Completed sql-aggregates exercise.

--- sql-aggregates/movie-maniacs.sql
+++ sql-aggregates/movie-maniacs.sql
@@ -1,10 +1,3 @@
--- List the first and last name of all customers, plus the total amount they've spent on rentals. Order them by total paid, descending.
-
--- customers: customerId, firstName, lastName
--- payments: customerId, paymentId, amount
--- rentals: rentalId, customerId, inventoryId
-
-
 SELECT "c"."firstName" as "First Name",
        "c"."lastName" as "Last Name",
       sum("amount") AS "Total Paid"
